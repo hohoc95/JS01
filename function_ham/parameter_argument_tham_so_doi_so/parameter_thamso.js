bf1a9cfb312f0fa1cde4dcd16c1435ebdc2a6ef1
hohoc95 <hohoc95@gmail.com>
2022-06-05 22:33:15 +0700

    hi

---
+++ function_ham/parameter_argument_tham_so_doi_so/parameter_thamso.js
@@ -0,0 +1,25 @@
+// Tham số hàm - Javascript cơ bản
+/*
+1. Tham số:
+    - Định nghĩa:Tham số là giá trị truyền vào khi gọi tới một hàm.
+    - Kiểu dữ liệu: Mọi kiểu dữ liệu (không giới hạn kiểu dữ liệu).
+    - Tính private: Một biến được sử dụng làm tham số trong functiom,
+         chỉ sử dụng được trong phạm vi khối mã {} của hàm đó mà thôi.
+    - Một tham số:
+    - Nhiều tham số:
+
+2. Truyền tham số:
+    - Một tham số:
+    - Nhiều tham số:
+3. Argument:
+    - Đối tượng argument:
+    - Giới thiệu vòng lặp for:
+*/
+
+
+function writelog(message) {
+    console.log(message);
+}
+writelog('test message');
+writelog('test message again');
+
