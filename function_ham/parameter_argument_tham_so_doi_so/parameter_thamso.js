ad967957d403fb5f022a025bcb6ee288f2fc3334
hohoc95 <hohoc95@gmail.com>
2022-06-06 15:30:21 +0700

    hi

--- function_ham/parameter_argument_tham_so_doi_so/parameter_thamso.js
+++ function_ham/parameter_argument_tham_so_doi_so/parameter_thamso.js
@@ -4,9 +4,12 @@
     - Định nghĩa:Tham số là giá trị truyền vào khi gọi tới một hàm.
     - Kiểu dữ liệu: Mọi kiểu dữ liệu (không giới hạn kiểu dữ liệu).
     - Tính private: Một biến được sử dụng làm tham số trong functiom,
-         chỉ sử dụng được trong phạm vi khối mã {} của hàm đó mà thôi.
-    - Một tham số:
-    - Nhiều tham số:
+        chỉ sử dụng được trong phạm vi khối mã {} của hàm đó mà thôi.
+        Tính private, và những phạm vi hoạt động của một biến, của một function chúng ta sẽ học ở
+        khóa nâng cao.
+    - Một tham số: gọi hàm, truyền 1 tham số.
+    - Nhiều tham số: truyền nhiều tham số thì có dấu phẩy ","
+        VD: writeLog('Test', 'Test_2)
 
 2. Truyền tham số:
     - Một tham số:
